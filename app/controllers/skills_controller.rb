9fd30b87f8ed26bb90970d79bff29d34d876f421
yutaka kobayashi <yutaka.kobayashi@todai-dream-net.com>
2017-03-29 15:33:34 +0000

    basic skill post feature

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,74 +1,36 @@
 class SkillsController < ApplicationController
-  before_action :set_skill, only: [:show, :edit, :update, :destroy]
-
-  # GET /skills
-  # GET /skills.json
   def index
     @skills = Skill.all
   end
 
-  # GET /skills/1
-  # GET /skills/1.json
   def show
+    @skill = Skill.find(params[:id])
   end
 
-  # GET /skills/new
   def new
-    @skill = Skill.new
   end
 
-  # GET /skills/1/edit
-  def edit
-  end
-
-  # POST /skills
-  # POST /skills.json
   def create
-    @skill = Skill.new(skill_params)
-
-    respond_to do |format|
-      if @skill.save
-        format.html { redirect_to @skill, notice: 'Skill was successfully created.' }
-        format.json { render :show, status: :created, location: @skill }
-      else
-        format.html { render :new }
-        format.json { render json: @skill.errors, status: :unprocessable_entity }
-      end
-    end
+    @skill = Skill.new
+    @skill.title = params[:title]
+    @skill.save
+    redirect_to skill_path(@skill.id)
   end
 
-  # PATCH/PUT /skills/1
-  # PATCH/PUT /skills/1.json
+  def edit
+    @skill = Skill.find(params[:id])
+  end
+
   def update
-    respond_to do |format|
-      if @skill.update(skill_params)
-        format.html { redirect_to @skill, notice: 'Skill was successfully updated.' }
-        format.json { render :show, status: :ok, location: @skill }
-      else
-        format.html { render :edit }
-        format.json { render json: @skill.errors, status: :unprocessable_entity }
-      end
-    end
+    @skill = Skill.find(params[:id])
+    @skill.title = params[:title]
+    @skill.save
+    redirect_to skill_path(@skill.id)
   end
 
-  # DELETE /skills/1
-  # DELETE /skills/1.json
   def destroy
+    @skill = Skill.find(params[:id])
     @skill.destroy
-    respond_to do |format|
-      format.html { redirect_to skills_url, notice: 'Skill was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to skills_path
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_skill
-      @skill = Skill.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def skill_params
-      params.require(:skill).permit(:title)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/top'
+
   resources :skills
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#top'
+  get '/about' => 'home#about'
 end
