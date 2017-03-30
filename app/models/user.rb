272d81245c4339863fa9b313e9e2029e04749d96
yutaka kobayashi <yutaka.kobayashi@todai-dream-net.com>
2017-03-30 06:04:58 +0000

    divise User made

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :validatable
   has_many :skills
   validates :name,presence:true
   validates :email,presence:true,uniqueness:true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330045012) do
+ActiveRecord::Schema.define(version: 20170330055202) do
 
   create_table "skills", force: :cascade do |t|
     t.string   "title"
@@ -22,9 +22,10 @@ ActiveRecord::Schema.define(version: 20170330045012) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "image"
+    t.string   "encrypted_password", default: "", null: false
   end
 
 end
