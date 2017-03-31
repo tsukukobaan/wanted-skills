170dfb7b8aa33556b86003e8f2eaf2b2630ef4e5
yutaka kobayashi <yutaka.kobayashi@todai-dream-net.com>
2017-03-31 16:47:36 +0000

    ajax on like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,13 @@
 class LikesController < ApplicationController
   def like
-    skill = Skill.find(params[:skill_id])
-    like = current_user.likes.build(skill_id: skill.id)
+    @skill = Skill.find(params[:skill_id])
+    like = current_user.likes.build(skill_id: @skill.id)
     like.save
-    redirect_to skill
   end
   
   def unlike
-    skill = Skill.find(params[:skill_id])
-    like = current_user.likes.find_by(skill_id: skill.id)
+    @skill = Skill.find(params[:skill_id])
+    like = current_user.likes.find_by(skill_id: @skill.id)
     like.destroy
-    redirect_to skill
   end
 end
